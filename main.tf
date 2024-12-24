ad65c199adeeb8e454aa25e58def296fd2888786
Safiyullah S A <sasafiyullah@outlook.com>
2024-12-24 20:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ resource "azurerm_subnet" "subnet1" {
 
 resource "azurerm_network_interface" "nic" {
   name                = "vmnic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
